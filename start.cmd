04afb2d4651c03d62269d396cd0294bbf90998dc
thenetworK1NG <thenetworking888@gmail.com>
2026-08-03 15:56:17 -0700

    Add files via upload

--- start.cmd
+++ start.cmd
@@ -1,1 +1,8 @@
-python -m http.server 8001
+@echo off
+rem QuickShare phone app is served by J.A.R.V.I.S. on port 8001 (login API + app).
+rem Make sure jarvis.py is running first, then open this page:
+start "" http://127.0.0.1:8001/wetransferphoneapp/
+echo.
+echo If the page fails to load, start J.A.R.V.I.S. first:
+echo     python jarvis.py
+echo On your phone, open:  http://<your-PC-IP>:8001/wetransferphoneapp/
